6e95401533907730983662be0d0f540defa5fb10
lrmanc <117472681+lrmanc@users.noreply.github.com>
2024-01-31 19:13:04 -0600

    controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,44 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
+    private Animator animator;
+
+    private float moveSpeed = 4f;
+
+    public float walkSpeed = 4f;
+    public float runSpeed = 8f;
 
     // Start is called before the first frame update
     void Start()
     {
-        controller = gameObject.AddComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
+        Move();
+    }
+
+    public void Move()
+    {
+        //Gets hor and vert inputs as numbers
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        //direction in normalized vector
+        Vector3 dir = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 velocity = moveSpeed * Time.deltaTime * dir;
+
+        //Detect sprint key
+        if (Input.GetButton("Sprint"))
         {
-            playerVelocity.y = 0f;
+            moveSpeed = runSpeed;
+            animator.SetBool("Running", true);
+        }
+        else
+        {
+            moveSpeed = walkSpeed;
+            animator.SetBool("Running", false);
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if(move != Vector3.zero)
+        //Detects movement
+        if(dir.magnitude >= 0.1f)
         {
-            gameObject.transform.forward = move;
+            //Looking toward a direction
+            transform.rotation = Quaternion.LookRotation(dir);
+
+            //Move
+            controller.Move(velocity);
+
         }
 
-        if(Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        //Speed parameter
+        animator.SetFloat("Speed", velocity.magnitude);
     }
 }
